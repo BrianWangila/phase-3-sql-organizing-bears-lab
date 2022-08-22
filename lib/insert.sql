ab7d1bee28fdb3b135d533817680e93a99ab77c9
Brian Wangila <bwanjalah@gmail.com>
2022-08-22 12:07:34 +0300

    done

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,23 +1,23 @@
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Mr. Chocolate', '42', 'M', 'white', 'stubborn', true);
+VALUES ('Mr. Chocolate', 42, 'M', 'white', 'stubborn', true);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Rowdy', '20', 'M', 'black', 'calm', false);
+VALUES ('Rowdy', 20, 'M', 'black', 'calm', false);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Tabitha', '30', 'F', 'white', 'stubborn', true);
+VALUES ('Tabitha', 30, F, 'white', 'stubborn', true);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Sergeant Brown', '15', 'M', 'Brown', 'stubborn', true);
+VALUES ('Sergeant Brown', 15, 'M', 'Brown', 'stubborn', true);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Melissa', '20', 'F', 'white', 'stubborn', true);
+VALUES ('Melissa', 20, 'F', 'white', 'stubborn', true);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Grinch', '20', 'M', 'grey', 'stubborn', true);
+VALUES ('Grinch', 20, 'M', 'grey', 'stubborn', true);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES ('Wendy', '10', 'F', 'stripped', 'stubborn', false);
+VALUES ('Wendy', 10, 'F', 'stripped', 'stubborn', false);
 
 INSERT INTO bears (name, age, sex, color, temperament, alive)
-VALUES (NULL, '20', 'M', 'maroon', 'stubborn', true);
+VALUES (NULL, 20, 'M', 'maroon', 'stubborn', true);
